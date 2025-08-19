f59d7f239659383b7f73f7d60c0933fd83a5d70b
ErickGiroto <erick_giroto@hotmail.com>
2025-08-19 11:28:29 -0300

    Critério de verificação da função em outra função

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ from pessoa import Pessoa
 
 p1 = Pessoa('Erick', 30) # p1(objeto) - Foi passado o valor nome(Erick), idade(30)
 
-# Solicitei que a pessoa iniciasse á comer!
-p1.comer('maçã') # Foi passado o valor do alimento(Maça)
-# Solicitei que a pessoa parasse de comer!
-p1.para_comer()
-
-print('-----------------')
-# Solicitei novamente a pessoa que iniciasse á comer!
-p1.comer('maçã') # Foi passado o valor do alimento(Maça)
-# Solicitei que a pessoa parasse de comer!
-p1.para_comer()
-# Solicitei que a pessoa parasse de comer novamente!
-p1.para_comer()
+p1.comer('maçã') # Solicitei que a pessoa iniciasse á comer!
+p1.falar('POO') # Solicitei que a pessoa que falasse!
+p1.para_comer() # Solicitei que a pessoa parasse de comer!
+p1.falar('POO') # Solicitei que a pessoa que falasse!
+p1.falar('POO') # Solicitei que a pessoa que falasse!
--- pessoa.py
+++ pessoa.py
@@ -9,6 +9,23 @@ class Pessoa:
         self.comendo = comendo
         self.falando = falando
 
+
+    # Manipulação com á variavel falando! ---------------------------------------
+    # Checando si a pessoa está comendo para poder falar!
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} não pode falar está comendo')
+            return
+
+        if self.falando:
+            print(f'{self.nome} já está falando sobre {assunto}.')
+            return
+
+        print(f'{self.nome} está falando sobre {assunto}.')
+        self.falando = True
+
+
+    # Manipulação com á variavel comendo! ---------------------------------------
     # Definindo status da função comer, mais que 01 vez
     def comer(self, alimento):
         if self.comendo: # Critério pra saber si é a 2 refeição
@@ -24,7 +41,7 @@ class Pessoa:
             print(f'{self.nome} não está comendo.')
             return
 
-        print(f'{self.nome} parou de comendo.')
+        print(f'{self.nome} parou de comer.')
         self.comendo = False
 
 
