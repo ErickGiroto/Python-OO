07ef5b8f3a48c2c9b724a3d43873941b8ba0dc82
ErickGiroto <erick_giroto@hotmail.com>
2025-08-19 10:25:44 -0300

    Revendo a criação de uma função

--- pessoa.py
+++ pessoa.py
@@ -1,6 +1,9 @@
+# Criando uma função, que recebe 02 argumentos e retorna a multiplicação de ambos.
+def multiplicacao(x, y):
+    return x * y
+
 # Criando um molde que representa uma pessoa como objeto
 # Necessario a criação de uma classe para esse molde
-
 
 class Pessoa:
     pass
