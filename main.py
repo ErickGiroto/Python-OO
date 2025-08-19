f5f434141e8259a229c9c20bad218f0fb3a53fd8
ErickGiroto <erick_giroto@hotmail.com>
2025-08-19 10:14:13 -0300

    Python criação de classe e identificacao do objeto

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pessoa import Pessoa
 
-# Tem que instanciar o cade objeto vindo do mesmo molde
-p1 = Pessoa()
-p2 = Pessoa()
+# Tem que instanciar cada objeto vindo do mesmo molde
+p1 = Pessoa() # p1 é o objeto
+p2 = Pessoa() # p2 é o objeto
 
 #p1 não é igual p2, veja o print abaixo
 print(p1)
