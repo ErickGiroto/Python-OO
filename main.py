3dd2687144cb6c692085e8034bb57d0ff55b4581
ErickGiroto <erick_giroto@hotmail.com>
2025-08-19 10:23:11 -0300

    As variavéis, são atributos(nome) da classe(p1)

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ p1 = Pessoa() # p1 é o objeto
 p2 = Pessoa() # p2 é o objeto
 
 #p1 não é igual p2, veja o print abaixo
-print(p1)
-print(p2)
+print("p1: ", p1) # objeto
+print("p2: ", p2) # objeto
+
+p1.nome = 'Erick'
+p2.nome = 'Joey'
+
+print(p1.nome) # As variavéis, são atributos(nome) da classe(p1)
+print(p2.nome)
