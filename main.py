9389e9aa4e18dd60037d90c3a08278afbb35f0f3
ErickGiroto <erick_giroto@hotmail.com>
2025-08-19 10:10:48 -0300

    Python criação de classe

---
+++ main.py
@@ -0,0 +1,9 @@
+from pessoa import Pessoa
+
+# Tem que instanciar o cade objeto vindo do mesmo molde
+p1 = Pessoa()
+p2 = Pessoa()
+
+#p1 não é igual p2, veja o print abaixo
+print(p1)
+print(p2)
---
+++ pessoa.py
@@ -0,0 +1,6 @@
+# Criando um molde que representa uma pessoa como objeto
+# Necessario a criação de uma classe para esse molde
+
+
+class Pessoa:
+    pass
