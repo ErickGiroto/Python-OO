f5620c24b0fc4c70a34ee40383e3a0ae60ffcdb0
ErickGiroto <erick_giroto@hotmail.com>
2025-08-19 14:51:33 -0300

    # Realizando metodos de classes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from pessoa import Pessoa
 
-p1 = Pessoa('Erick', 30) # p1(objeto) - Foi passado o valor nome(Erick), idade(30)
-p2 = Pessoa('Vanessa', 32)
+# Realizando metodos de classes
 
+# p1 = Pessoa.por_ano_nascimento('Erick', 1995) # Opção 01
+p1 = Pessoa('Erick', 1995) # Opção 02
+print(p1)
 
-print(p1.get_ano_nascimento()) # Buscando a função dentro da classe, junto com o objeto
-print(p2.get_ano_nascimento()) # Buscando a função dentro da classe, junto com o objeto
+print(p1.nome, p1.idade, 'anos')
+
